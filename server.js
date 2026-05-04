d3093f2171cbf067f1023b2f31a6c52d4e651f77
socialsound99 <socialsound99@gmail.com>
2026-05-04 22:10:34 +0530

    server.js

--- server.js
+++ server.js
@@ -50,4 +50,5 @@ Description:
   res.json(results);
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
